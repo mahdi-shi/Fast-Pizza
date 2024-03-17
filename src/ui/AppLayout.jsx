07ef971ee63c8db846b076d57637d4a21744e6e4
Tailus Dev <4932abcd@gmail.com>
2024-03-17 14:14:48 +0330

    new feature, layout loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,17 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div>
-        <Header />
+      {isLoading && <Loader />}
+
+      <Header />
 
       <main>
         <Outlet />
