9c5c09f2541e37cb22ab0f1b88b162d849dc6fd4
Tailus Dev <4932abcd@gmail.com>
2024-03-24 18:27:43 +0330

    requirement fearure, dispatch username state in home page

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -24,7 +24,7 @@ async function fetchAddress() {
 */
 
 const initialState = {
-  username: "Tailus",
+  username: "",
 };
 
 const userReducer = createSlice({
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,16 +1,28 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className="py-6 text-center text-base sm:text-lg">
-      <h1 className="text-center text-xl px-1 font-bold font-semibold text-stone-600 sm:text-2xl">
+      <h1 className="px-1 text-center text-xl font-bold font-semibold text-stone-600 sm:text-2xl">
         The best pizza.
         <br />
         <span className="text-yellow-500 sm:text-3xl">
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      <div className="mt-10">
+        {!username ? (
+          <CreateUser />
+        ) : (
+          <Button to="/menu" type="primary">
+            Continuing Order, {username}
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
